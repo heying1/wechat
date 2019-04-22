060656b57393960d0bd6559940ce6e32b4b29e0b
student <student@hubu.edu.cn>
2019-04-22 17:36:33 +0800

    123

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,57 @@
 <template>
   <div>
-    <i-notice-bar>
+    <i-notice-bar icon="systemprompt" loop>
     好好学习，天天向上！
     </i-notice-bar>
+
+    <i-row i-class="have-border">
+    <i-col span="8" i-class="have-border" offset="4">
+      col-8
+      <i-button bind:click="handleClick" type="success" shape="circle">默认尺寸</i-button>
+
+    </i-col>
+
+    <i-col span="8" i-class="have-border" offset="4">
+      col-8
+      <i-button bind:click="handleClick" type="success" shape="circle">默认尺寸</i-button>
+
+
+      
+
+    </i-col>
+    </i-row>
+
     <i-grid i-class="no-border">
-    <i-grid-item>  
+    <i-grid-item i-class="no-border">  
          <i-grid-icon>
             <image src="/static/images/music.png" />
         </i-grid-icon> 
          <i-grid-label>乐器学习</i-grid-label>
     </i-grid-item>
 
-    <i-grid-item>  
+    <i-grid-item i-class="no-border">  
          <i-grid-icon>
             <image src="/static/images/sport.png" />
         </i-grid-icon> 
          <i-grid-label>体育运动</i-grid-label>
-    </i-grid-item>
-     
+    </i-grid-item> 
 </i-grid>
+
 <i-grid i-class="no-border">
-    <i-grid-item>  
+    <i-grid-item i-class="no-border">  
          <i-grid-icon>
             <image src="/static/images/exam.png" />
         </i-grid-icon> 
          <i-grid-label>考试培训</i-grid-label>
     </i-grid-item>
-    <i-grid-item>  
+    <i-grid-item i-class="no-border">  
          <i-grid-icon>
             <image src="/static/images/lang.png" />
         </i-grid-icon> 
          <i-grid-label>语种学习</i-grid-label>
     </i-grid-item>
 </i-grid>
+
 <view v-for="item in list" :key='item' class="top-padding">
  <i-card :title="item.name" :extra="item.id" :thumb="item.photo">
     <view slot="content">{{item.major}}</view>
@@ -98,5 +117,8 @@ export default {
 <style scoped>
 div >>>.no-border{
   border-width: 0pt;
+}
+div >>>.have-border{
+  border-width: 1pt;
 }
 </style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -11,11 +11,7 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      
     }
   },
 
@@ -24,75 +20,15 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
+    
   },
 
   created () {
-    // let app = getApp()
+   
   }
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+<style >
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
